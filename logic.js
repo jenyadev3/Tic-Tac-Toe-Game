a41051363666e681829ce592742f7d38fd4148de
Evgeniya <112981213+jenyadev3@users.noreply.github.com>
2023-05-06 13:45:03 +0300

    Update logic.js
    Код доработан таким образом чтобы работал при доске любого размера

--- logic.js
+++ logic.js
@@ -1,50 +1,37 @@
-
 let activePlayer;
 let secondPlayer;
-let winner;
 let board;
 
 function startGame() {
   board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
+    ['', '', '', ''],
+    ['', '', '', ''],
+    ['', '', '', ''],
+    ['', '', '', '']
   ];
   activePlayer = 'x';
   secondPlayer = 'o';
   renderBoard(board);   
 }
 
-
 function click(row, col) {
-
   board[row][col] = activePlayer; 
   renderBoard(board);  
+  let column = board.map(row => row[col]);
+  let diag1 = board.map((row, i) => row[i]);
+  let diag2 = board.map((row, i) => row[board.length - 1 - i]);
 
-  if (board[0][0] === 'x' && board[0][1] === 'x' && board[0][2] === 'x'||
-    board[1][0] === 'x' && board[1][1] === 'x' && board[1][2] === 'x' ||
-    board[2][0] === 'x' && board[2][1] === 'x' && board[2][2] === 'x' ||
-    board[0][0] === 'x' && board[1][0] === 'x' && board[2][0] === 'x' ||
-    board[0][1] === 'x' && board[1][1] === 'x' && board[2][1] === 'x' ||
-    board[0][2] === 'x' && board[1][2] === 'x' && board[2][2] === 'x' ||
-    board[0][0] === 'x' && board[1][1] === 'x' && board[2][2] === 'x' ||
-    board[0][2] === 'x' && board[1][1] === 'x' && board[2][0] === 'x') {
+  if ((board[row].every(element => element === 'x')) || (column.every(element => element === 'x')) || (diag1.every(element => element === 'x')) || (diag2.every(element => element === 'x'))) {
     showWinner('1');
-    } else if 
-      (board[0][0] === 'o' && board[0][1] === 'o' && board[0][2] === 'o'||
-      board[1][0] === 'o' && board[1][1] === 'o' && board[1][2] === 'o' ||
-      board[2][0] === 'o' && board[2][1] === 'o' && board[2][2] === 'o' ||
-      board[0][0] === 'o' && board[1][0] === 'o' && board[2][0] === 'o' ||
-      board[0][1] === 'o' && board[1][1] === 'o' && board[2][1] === 'o' ||
-      board[0][2] === 'o' && board[1][2] === 'o' && board[2][2] === 'o' ||
-      board[0][0] === 'o' && board[1][1] === 'o' && board[2][2] === 'o' ||
-      board[0][2] === 'o' && board[1][1] === 'o' && board[2][0] === 'o'){
-      showWinner('2'); 
-    } else { //смена хода
-    let tempPlayer = activePlayer;
-    activePlayer = secondPlayer;
-    secondPlayer = tempPlayer;
-  }
+  }  
+  else if ((board[row].every(element => element === 'o')) || (column.every(element => element === 'o')) || (diag1.every(element => element === 'o')) || (diag2.every(element => element === 'o'))) {
+    showWinner('2');
+  } else {
+  // Меняем игроков, если никто не выиграл
+  let tempPlayer = activePlayer;
+  activePlayer = secondPlayer;
+  secondPlayer = tempPlayer;    
+  }  
 }
 
 
